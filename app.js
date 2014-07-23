90a05b2493a448bd62d7ebbd031505e21b524a90
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2014-07-23 22:27:06 +0200

    First droppable implementation

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
   $('li img').draggable({
     helper: function () {
       var $this = $(this);
@@ -9,4 +11,34 @@ $(document).ready(function () {
       return originalImage;
     }
   });
+
+  var getRelativePosition = function ($element1, $element2) {
+    var o1 = $element1.offset();
+    var o2 = $element2.offset();
+    var dx = o1.left - o2.left;
+    var dy = o1.top - o2.top;
+    return {
+      top: dy,
+      left: dx
+    };
+  };
+
+  $('canvas').droppable({
+    accept: 'img',
+    tolerance: 'fit',
+    drop: function (event, ui) {
+      var element = new createjs.Bitmap(ui.helper.get(0));
+      stage.addChild(element);
+      var position = getRelativePosition(ui.helper, $('canvas'));
+      element.x = position.left;
+      element.y = position.top;
+      stage.update();
+      ui.helper.remove();
+    }
+  });
+
+  $('canvas').attr('width', $('.right-panel').width())
+    .attr('height', $('.right-panel').height());
+
+  var stage = new createjs.Stage('stage');
 });
