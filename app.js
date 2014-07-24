2b78ad99730b9005ae92f364f49f6bf4812dbf20
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2014-07-24 22:13:36 +0200

    Some refactor, added class EditorElement

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
 $(document).ready(function () {
 
+  var EditorElement = function (imageSrc) {
+    var texture = PIXI.Texture.fromImage(imageSrc);
+    this._pixiSprite = new PIXI.Sprite(texture);
+
+    this._pixiSprite.anchor.x = 0.5;
+    this._pixiSprite.anchor.y = 0.5;
+    this._pixiSprite.interactive = true;
+    this._pixiSprite.buttonMode = true;
+
+    this._pixiSprite.mousedown = EditorElement._mouseDownHandler.bind(this);
+    this._pixiSprite.mouseup = EditorElement._mouseUpHandler.bind(this);
+
+    stage.addChild(this._pixiSprite);
+  };
+
+  EditorElement._mouseDownHandler = function (data) {
+    this._dragging = true;
+    this._pixiSprite.data = data;
+
+    var localPosition = this._pixiSprite.data.getLocalPosition(this._pixiSprite);
+
+    this._sx = localPosition.x;
+    this._sy = localPosition.y;
+
+    this._pixiSprite.mousemove = EditorElement._mouseMoveHandler.bind(this);
+  };
+
+  EditorElement._mouseMoveHandler = function () {
+    if(this._dragging)
+    {
+      var newPosition = this._pixiSprite.data.getLocalPosition(this._pixiSprite.parent);
+      this._pixiSprite.position.x = newPosition.x - this._sx;
+      this._pixiSprite.position.y = newPosition.y - this._sy;
+    }
+  };
+
+  EditorElement._mouseUpHandler = function () {
+    this._dragging = false;
+    this._pixiSprite.data = null;
+    delete this._pixiSprite.mousemove;
+  };
+
+  EditorElement.prototype.setPosition = function (position) {
+    this._pixiSprite.position.x = position.x;
+    this._pixiSprite.position.y = position.y;
+  }
 
   $('li img').draggable({
     helper: function () {
@@ -27,47 +73,14 @@ $(document).ready(function () {
     accept: 'img',
     tolerance: 'fit',
     drop: function (event, ui) {
-      var texture = PIXI.Texture.fromImage(ui.helper.eq(0).attr('src'));
-      var element = new PIXI.Sprite(texture);
-      element.interactive = true;
-      element.buttonMode = true;
-
-      element.anchor.x = 0.5;
-      element.anchor.y = 0.5;
-
-      element.mousedown = function (data) {
-        this.dragging = true;
-        this.data = data;
-        this.sx = this.data.getLocalPosition(this).x;
-        this.sy = this.data.getLocalPosition(this).y;
-
-        this.mousemove = function(data)
-        {
-          if(this.dragging)
-          {
-            var newPosition = this.data.getLocalPosition(this.parent);
-            this.position.x = newPosition.x - this.sx;
-            this.position.y = newPosition.y - this.sy;
-
-            //console.log(this.position.x, this.position.y);
-          }
-
-          console.log('dupda')
-        };
-      };
-
-      element.mouseup = function () {
-        this.dragging = false;
-        this.data = null;
-        delete this.mousemove;
-      };
-
+      var element = new EditorElement(ui.helper.eq(0).attr('src'));
       var position = getRelativePosition(ui.helper, $('canvas'));
-      element.position.x = position.left + ui.helper.width()/2;
-      element.position.y = position.top + ui.helper.height()/2;
+      element.setPosition({
+        x: position.left + ui.helper.width()/2,
+        y: position.top + ui.helper.height()/2
+      });
 
       ui.helper.remove();
-      stage.addChild(element);
     }
   });
 
