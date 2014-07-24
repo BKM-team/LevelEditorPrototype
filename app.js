81fe2a1f7367a7df246be76edb5ff4084f3f558f
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2014-07-24 20:35:35 +0200

    Switched to Pixi.js, as it's a lot faster than EaselJS

--- app.js
+++ app.js
@@ -27,34 +27,52 @@ $(document).ready(function () {
     accept: 'img',
     tolerance: 'fit',
     drop: function (event, ui) {
-      var element = new createjs.Bitmap(ui.helper.get(0));
-      container.addChild(element);
+      var texture = PIXI.Texture.fromImage(ui.helper.eq(0).attr('src'));
+      var element = new PIXI.Sprite(texture);
+      element.interactive = true;
+      element.buttonMode = true;
+
+      element.anchor.x = 0.5;
+      element.anchor.y = 0.5;
+
+      element.mousedown = function (data) {
+        this.dragging = true;
+        //this.data = data;
+      };
+
+      element.mouseup = function () {
+        this.dragging = false;
+      };
+
+      element.mousemove = function(data)
+      {
+        if(this.dragging)
+        {
+          var newPosition = data.getLocalPosition(this.parent);
+          this.position.x = newPosition.x;
+          this.position.y = newPosition.y;
+        }
+      };
+
       var position = getRelativePosition(ui.helper, $('canvas'));
-      element.x = position.left;
-      element.y = position.top;
-      stage.update();
-      element.cursor = "pointer";
+      element.position.x = position.left + ui.helper.width()/2;
+      element.position.y = position.top + ui.helper.height()/2;
+
       ui.helper.remove();
-
-      element.on("mousedown", function(evt) {
-        this.offset = {x:this.x-evt.stageX, y:this.y-evt.stageY};
-      });
-
-      element.on("pressmove", function(evt) {
-        this.x = evt.stageX+ this.offset.x;
-        this.y = evt.stageY+ this.offset.y;
-      });
+      stage.addChild(element);
     }
   });
 
   $('canvas').attr('width', $('.right-panel').width())
     .attr('height', $('.right-panel').height());
 
-  var stage = new createjs.Stage('stage');
-  var container = new createjs.Container();
-  stage.addChild(container);
-  stage.enableMouseOver(10);
-  createjs.Ticker.addEventListener("tick", function () {
-    stage.update();
-  });
+  var stage = new PIXI.Stage();
+  var renderer = PIXI.autoDetectRenderer($('.right-panel').width(), $('.right-panel').height(), $('canvas').get(0));
+
+  function animate() {
+    requestAnimFrame( animate );
+    renderer.render(stage);
+  }
+
+  requestAnimFrame(animate);
 });
