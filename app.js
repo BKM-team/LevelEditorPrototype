f508c75ca1deaab14d2ac65269cbbc3b6ade58b0
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2014-07-26 18:51:12 +0200

    Major refactor, added Stage wrapper to createjs.Stage

--- app.js
+++ app.js
@@ -1,55 +1,109 @@
 'use strict';
 
+$.fn.posRelativeTo = function (element) {
+  var thisOffset = this.offset();
+  var elementOffset = element.offset();
+  var dx = thisOffset.left - elementOffset.left;
+  var dy = thisOffset.top - elementOffset.top;
+  return {
+    top: dy,
+    left: dx
+  };
+};
+
+var EditorElement = function (imageSrc) {
+  this._sprite = new createjs.Bitmap(imageSrc);
+
+  this._sprite.on('mousedown', EditorElement._mouseDownHandler.bind(this));
+  this._sprite.on('pressmove', EditorElement._mouseMoveHandler.bind(this));
+  this._sprite.cursor = 'pointer';
+
+  Object.defineProperty(this, 'x', EditorElement._xGetSet);
+  Object.defineProperty(this, 'y', EditorElement._yGetSet);
+};
+
+EditorElement._xGetSet = {
+  get: function () {
+    return this._sprite.x;
+  },
+  set: function (val) {
+    this._sprite.x = val;
+  }
+};
+
+EditorElement._yGetSet = {
+  get: function () {
+    return this._sprite.y;
+  },
+  set: function (val) {
+    this._sprite.y = val;
+  }
+};
+
+EditorElement._mouseDownHandler = function (evt) {
+  this._dragStartPosition = {
+    x: this.x - evt.stageX,
+    y: this.y - evt.stageY
+  };
+};
+
+EditorElement._mouseMoveHandler = function (evt) {
+  this.x = evt.stageX + this._dragStartPosition.x;
+  this.y = evt.stageY + this._dragStartPosition.y;
+};
+
+EditorElement.prototype.setPosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
+
+EditorElement.prototype.getSprite = function () {
+  return this._sprite;
+};
+
+var Stage = function ($canvas) {
+  this._canvas = $canvas;
+  this._stage = new createjs.Stage(this._canvas.attr('id'));
+  this._stage.enableMouseOver(10);
+};
+
+Stage.prototype.updateSize = function () {
+  var $canvasParent = this._canvas.parent();
+  this._canvas.attr('width', $canvasParent.width());
+  this._canvas.attr('height', $canvasParent.height());
+};
+
+Stage.prototype.installEvents = function () {
+  this._canvas.droppable({
+    tolerance: 'fit',
+    drop: (function (event, ui) {
+      var element = new EditorElement(ui.helper.eq(0).attr('src'));
+      this._stage.addChild(element.getSprite());
+
+      var position = ui.helper.posRelativeTo(this._canvas);
+
+      element.setPosition({
+        x: position.left,
+        y: position.top
+      });
+
+      ui.helper.remove();
+    }).bind(this)
+  });
+
+  createjs.Ticker.on('tick', Stage._tickHandler.bind(this));
+  $(window).on('resize', Stage._resizeHandler.bind(this));
+};
+
+Stage._resizeHandler = function () {
+  this.updateSize();
+};
+
+Stage._tickHandler = function () {
+  this._stage.update();
+};
+
 $(document).ready(function () {
-
-  var EditorElement = function (imageSrc) {
-    this._sprite = new createjs.Bitmap(imageSrc);
-
-    this._sprite.on('mousedown', EditorElement._mouseDownHandler.bind(this));
-    this._sprite.on('pressmove', EditorElement._mouseMoveHandler.bind(this));
-    this._sprite.cursor = 'pointer';
-
-    Object.defineProperty(this, 'x', EditorElement._xGetSet);
-    Object.defineProperty(this, 'y', EditorElement._yGetSet);
-
-    stage.addChild(this._sprite);
-  };
-
-  EditorElement._xGetSet = {
-    get: function () {
-      return this._sprite.x;
-    },
-    set: function (val) {
-      this._sprite.x = val;
-    }
-  };
-
-  EditorElement._yGetSet = {
-    get: function () {
-      return this._sprite.y;
-    },
-    set: function (val) {
-      this._sprite.y = val;
-    }
-  };
-
-  EditorElement._mouseDownHandler = function (evt) {
-    this._dragStartPosition = {
-      x: this.x - evt.stageX,
-      y: this.y - evt.stageY
-    };
-  };
-
-  EditorElement._mouseMoveHandler = function (evt) {
-    this.x = evt.stageX + this._dragStartPosition.x;
-    this.y = evt.stageY + this._dragStartPosition.y;
-  };
-
-  EditorElement.prototype.setPosition = function (position) {
-    this.x = position.x;
-    this.y = position.y;
-  };
-
   $('li').draggable({
     helper: function () {
       var $img = $(this).find('img');
@@ -62,46 +116,10 @@ $(document).ready(function () {
     }
   });
 
-  $.fn.posRelativeTo = function (element) {
-    var thisOffset = this.offset();
-    var elementOffset = element.offset();
-    var dx = thisOffset.left - elementOffset.left;
-    var dy = thisOffset.top - elementOffset.top;
-    return {
-      top: dy,
-      left: dx
-    };
-  };
+  var stage = new Stage($('#stage'));
+  stage.installEvents();
+  stage.updateSize();
 
-  $('canvas').droppable({
-    tolerance: 'fit',
-    drop: function (event, ui) {
-      var element = new EditorElement(ui.helper.eq(0).attr('src'));
-      var position = ui.helper.posRelativeTo($('#stage'));
-
-      element.setPosition({
-        x: position.left,
-        y: position.top
-      });
-
-      ui.helper.remove();
-    }
-  });
-
-  $('canvas').attr('width', $('.right-panel').width())
-    .attr('height', $('.right-panel').height());
-
-  $(window).on('resize', function () {
-    $('canvas').attr('width', $('.right-panel').width())
-      .attr('height', $('.right-panel').height());
-  });
-
-  var stage = new createjs.Stage('stage');
-  stage.enableMouseOver(10);
-
-  createjs.Ticker.on('tick', function () {
-    stage.update();
-  });
   createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
   createjs.Ticker.setFPS(60);
 });
